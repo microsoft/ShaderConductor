4b52f0e50f2df783a994e7c9f9a30f45dd1ffab7
Jorge Cantón <jorgemagic@gmail.com>
2018-12-05 11:52:46 +0100

    - Added CSharpPinvoke test project

---
+++ Source/Wrapper/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace CSharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = File.ReadAllText("shader.hlsl");
+
+            Wrapper.SourceDesc sourceDesc = new Wrapper.SourceDesc()
+            {
+                entryPoint = "VS",
+                stage = Wrapper.ShaderStage.VertexShader,
+                source = source,
+            };
+
+            Wrapper.TargetDesc targetDesc = new Wrapper.TargetDesc()
+            {
+                language = Wrapper.ShadingLanguage.Glsl,
+                version = "450",
+            };
+
+            Wrapper.Compile(ref sourceDesc, ref targetDesc, out Wrapper.ResultDesc result);
+            string translate = Marshal.PtrToStringAnsi(result.target);
+            string warning = Marshal.PtrToStringAnsi(result.errorWarningMsg);
+
+            Console.WriteLine("*************************\n" +
+                              "**  GLSL translation   **\n" +
+                              "*************************\n");
+            Console.WriteLine(translate);
+            Console.WriteLine("*************************\n"+
+                              "**  Error output       **\n"+
+                              "*************************\n");
+            Console.WriteLine(warning);
+
+            Console.Read();
+        }
+    }
+}
---
+++ Source/Wrapper/Wrapper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConsole
+{
+    public class Wrapper
+    {     
+        [DllImport("ShaderConductorWrapper.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Compile([In] ref SourceDesc source, [In] ref TargetDesc target, out ResultDesc result);      
+
+        public enum ShaderStage
+        {
+            VertexShader,
+            PixelShader,
+            GeometryShader,
+            HullShader,
+            DomainShader,
+            ComputeShader,
+
+            NumShaderStages,
+        };
+
+        public enum ShadingLanguage
+        {
+            Dxil = 0,
+            SpirV,
+
+            Hlsl,
+            Glsl,
+            Essl,
+            Msl,
+
+            NumShadingLanguages,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MacroDefine
+        {
+            public string name;
+            public string value;
+        };
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct SourceDesc
+        {
+            public string source;            
+            public string entryPoint;
+            public ShaderStage stage;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct TargetDesc
+        {
+            public ShadingLanguage language;
+            public string version;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ResultDesc
+        {
+            public bool hasError;
+            public IntPtr target;
+            public IntPtr errorWarningMsg;            
+        }
+    }
+}
